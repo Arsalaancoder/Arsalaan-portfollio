886d231c04166698de107fadb2e424945a6713a1
Arsalaancoder <154648662+Arsalaancoder@users.noreply.github.com>
2024-01-17 22:46:07 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,8 @@
+  // tying animation
+  var typed = new Typed(".typing",{
+    strings:["web Developer","UI Designer","Wordpress Developer"],
+    typespeed:30,
+    backspeed:20,
+    loop:true
+
+  })
